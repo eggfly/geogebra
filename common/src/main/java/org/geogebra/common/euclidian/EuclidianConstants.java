fff83b417c67338f438dc117f7d478fe6644cb77
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2021-02-10 10:15:18 +0100

    MOW-1148 add mindmap tool

--- common/src/main/java/org/geogebra/common/euclidian/EuclidianConstants.java
+++ common/src/main/java/org/geogebra/common/euclidian/EuclidianConstants.java
@@ -929,6 +929,8 @@ public final class EuclidianConstants {
 			return "Table";
 		case EuclidianConstants.MODE_EQUATION:
 			return "Equation";
+		case EuclidianConstants.MODE_MIND_MAP:
+			return "Mindmap";
 
 		default:
 			Log.error("Unknown mode " + mode);
